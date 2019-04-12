8b30b74c13d364280f9002975a1b74ef108bd989
Citlalli Prado <citlalli.prado@gmail.com>
2019-04-12 23:24:11 +0200

    Changed script.js to spit very long function into more functions. Starting specs for each function

---
+++ jasmine/spec/scriptSpec.js
@@ -0,0 +1,29 @@
+const utils = require("../src/js/script.js");
+
+describe("Search", function() {
+  
+  it("should get the searched element", function() {
+    expect().nothing; //getSearchedElement
+  });
+
+  it("should call the api", function() {
+    expect().nothing; //callApi
+  });
+
+  it("should return a result if clicked", function() {
+    expect().nothing;//show results
+  });
+
+  it("should return a user if exists", function() {
+    expect().nothing;//show user info
+  });
+  
+  it("should return repos if user exists", function() {
+    expect().nothing;//showRepos
+  });
+  
+  it("should return error if user does not exists", function() {
+    expect().nothing;//showError
+  });
+  
+});
--- jasmine/src/js/script.js
+++ jasmine/src/js/script.js
@@ -2,53 +2,18 @@ var request = new XMLHttpRequest();
 var request2 = new XMLHttpRequest();
 var user = "citlas"
 
-function searchThis(){
-    document.getElementById('avatar').innerHTML = "";
-    document.getElementById('nameInfo').innerHTML = "";
-    document.getElementById('repos').innerHTML = "";
-    document.getElementById('error').innerHTML = "";
-  
-    //Get the searched element
-    var searching = document.getElementById("searchedText").value;
-    console.log(searching)
 
-    //Call the api for user searched
-    request.open('GET', `https://api.github.com/users/${searching}`, true);
-    request.onload = function(){
-        var data = JSON.parse(this.response);
-        //console.log(data)
+function showError(){
+    //error
+    const errorDiv = document.getElementById('error');
+    const error = document.createElement('p');
+    error.textContent = "Does not exist";
+    error.setAttribute('class','error');
+    errorDiv.appendChild(error);
+}
 
-    //Show results if success
-    if(request.status >= 200 && request.status < 400){
-        const app = document.getElementById('info');
-        app.setAttribute('class','show');
-        
-        //avatar image
-        const avatarDiv = document.getElementById('avatar');    
-        const avatar1 = document.createElement('img');
-        avatar1.src = data.avatar_url;
-        avatar1.setAttribute('class','avatar');
-        avatarDiv.appendChild(avatar1);
-        
-        //username
-        const nameInfo = document.getElementById('nameInfo');
-        const username = document.createElement('h6');
-        username.textContent = '@'+data.login;
-        username.setAttribute('class','username');
-        nameInfo.appendChild(username);
 
-        //fullname
-        const fullname = document.createElement('h1');
-        fullname.textContent = data.name;
-        fullname.setAttribute('class','fullname');
-        nameInfo.appendChild(fullname);
-
-        //bio
-        const bio = document.createElement('h5');
-        bio.textContent = data.bio;
-        bio.setAttribute('class','bio');
-        nameInfo.appendChild(bio);
-        
+function showRepos(searching){
 
         //calling to get their repos
         request2.open('GET', `https://api.github.com/users/${searching}/repos`, true);
@@ -111,22 +76,83 @@ function searchThis(){
             
 
         }
+
+}
+
+
+function showUserInfo(data){
+    const app = document.getElementById('info');
+    app.setAttribute('class','show');
+    
+    //avatar image
+    const avatarDiv = document.getElementById('avatar');    
+    const avatar1 = document.createElement('img');
+    avatar1.src = data.avatar_url;
+    avatar1.setAttribute('class','avatar');
+    avatarDiv.appendChild(avatar1);
+    
+    //username
+    const nameInfo = document.getElementById('nameInfo');
+    const username = document.createElement('h6');
+    username.textContent = '@'+data.login;
+    username.setAttribute('class','username');
+    nameInfo.appendChild(username);
+
+    //fullname
+    const fullname = document.createElement('h1');
+    fullname.textContent = data.name;
+    fullname.setAttribute('class','fullname');
+    nameInfo.appendChild(fullname);
+
+    //bio
+    const bio = document.createElement('h5');
+    bio.textContent = data.bio;
+    bio.setAttribute('class','bio');
+    nameInfo.appendChild(bio);
+}
+
+function showResults(data,searching){
+
+    //Show results if success
+    if(request.status >= 200 && request.status < 400){
+        showUserInfo(data);
+        
+        showRepos(searching);
+        
         request2.send();
          
             
         } else {
 
-            //error
-            const errorDiv = document.getElementById('error');
-            const error = document.createElement('p');
-            error.textContent = "Does not exist";
-            error.setAttribute('class','error');
-            errorDiv.appendChild(error);
+          showError()
     }
-    
-    
 }
-request.send();
-/*
-*/
+
+function callApi(searching){
+    //Call the api for user searched
+    request.open('GET', `https://api.github.com/users/${searching}`, true);
+    request.onload = function(){
+    var data = JSON.parse(this.response);
+
+    //console.log(data)
+
+    showResults(data,searching);    
+    }
+    request.send();
+}
+
+function getSearchedElement(){
+    //Get the searched element
+    var searching = document.getElementById("searchedText").value;
+    //console.log(searching)
+    callApi(searching);
+}
+
+function searchThis(){
+    document.getElementById('avatar').innerHTML = "";
+    document.getElementById('nameInfo').innerHTML = "";
+    document.getElementById('repos').innerHTML = "";
+    document.getElementById('error').innerHTML = "";
+  
+    getSearchedElement();
 }
